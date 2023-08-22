fefa20410bf948b97041e6e422a60c9e43300136
Home <mapariabdullah@gmail.com>
2023-08-22 15:43:05 +0530

    improved print statements in main.py

--- main.py
+++ main.py
@@ -27,15 +27,5 @@ if __name__ == "__main__":
     end = timeit.default_timer()
 
     print(f'\nAnswer: {response["result"]}')
-    # print('='*50)
-
-    # Process source documents
-    # source_docs = response['source_documents']
-    # for i, doc in enumerate(source_docs):
-    #     print(f'\nSource Document {i+1}\n')
-    #     print(f'Source Text: {doc.page_content}')
-    #     print(f'Document Name: {doc.metadata["source"]}')
-    #     print(f'Page Number: {doc.metadata["page"]}\n')
-    #     print('='* 60)
 
     print(f"Time to retrieve response: {end - start}")
